bb543c4a39fdd8d1f254e022453d350fef96537c
mojtaba <ghezel1361@yahoo.com>
2021-08-03 10:49:19 +0200

    components part 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./components/Menu";
 
+import { useState } from "react";
+import { DISHES } from "./shared/dishes";
+
 function App() {
+  const [dishes, setDishes] = useState(DISHES);
   return (
     <div>
       <Navbar dark color="primary">
@@ -10,7 +14,7 @@ function App() {
           <NavbarBrand href="/">Restaurant Confusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={dishes} />
     </div>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,69 +1,47 @@
 import React from "react";
 import { useState } from "react";
-import { Media } from "reactstrap";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+} from "reactstrap";
 
-const Menu = () => {
-  const [dishes, setDishes] = useState([
-    {
-      id: 0,
-      name: "Uthappizza",
-      image: "assets/images/uthappizza.png",
-      category: "mains",
-      label: "Hot",
-      price: "4.99",
-      description:
-        "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
-    },
-    {
-      id: 1,
-      name: "Zucchipakoda",
-      image: "assets/images/zucchipakoda.png",
-      category: "appetizer",
-      label: "",
-      price: "1.99",
-      description:
-        "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-    },
-    {
-      id: 2,
-      name: "Vadonut",
-      image: "assets/images/vadonut.png",
-      category: "appetizer",
-      label: "New",
-      price: "1.99",
-      description:
-        "A quintessential ConFusion experience, is it a vada or is it a donut?",
-    },
-    {
-      id: 3,
-      name: "ElaiCheese Cake",
-      image: "assets/images/elaicheesecake.png",
-      category: "dessert",
-      label: "",
-      price: "2.99",
-      description:
-        "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-    },
-  ]);
+const Menu = ({ dishes }) => {
+  const [selectedDish, setSelectedDish] = useState(null);
+  //   render dish
+  const renderDish = (dish) => {
+    if (dish !== null) {
+      return (
+        <Card onClick={() => setSelectedDish(null)}>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  };
   //   Menu component
   const menu = dishes.map((dish) => (
-    <div key={dish.id} className="col-12 mt-5">
-      <Media tag="li">
-        <Media left middle>
-          <Media object src={dish.image} alt={dish.name} />
-        </Media>
-        <Media body className="ml-5">
-          <Media heading>{dish.name}</Media>
-          <p>{dish.description}</p>
-        </Media>
-      </Media>
+    <div key={dish.id} className="col-12 col-md-6 mt-md-4">
+      <Card onClick={() => setSelectedDish(dish)}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Card>
     </div>
   ));
   return (
     <div className="container">
-      <div className="row">
-        <Media list>{menu}</Media>
-      </div>
+      <div className="row">{menu}</div>
+      <div className="row m-3">{renderDish(selectedDish)}</div>
     </div>
   );
 };
